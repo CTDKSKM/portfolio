f167c1a74d05915ee3096e4b7918e0c362ba9c48
CTDKSKM <skmdldy001@gmail.com>
2023-12-21 17:15:35 +0900

    skills content update

--- src/components/Home/Skills.tsx
+++ src/components/Home/Skills.tsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import ArticleHeader from './ArticleHeader';
-
+import SKILL_IMGS from '../../static/images/skills/index';
 type Props = {};
 
 const Skills = (props: Props) => {
   const skillContainers = [
-    { value: 'Frontend', imgUrl: 'logo192.png' },
-    { value: 'Backend', imgUrl: 'logo192.png' },
-    { value: 'Deployment', imgUrl: 'logo192.png' },
-    { value: 'Version Control', imgUrl: 'logo192.png' },
-    { value: 'Communication', imgUrl: 'logo192.png' }
+    { value: 'Frontend', imgUrl: SKILL_IMGS['frontend'] },
+    { value: 'Backend', imgUrl: SKILL_IMGS['backend'] },
+    { value: 'Deployment', imgUrl: SKILL_IMGS['deployment'] },
+    { value: 'Version Control', imgUrl: SKILL_IMGS['versionControl'] },
+    { value: 'Communication', imgUrl: SKILL_IMGS['communication'] }
   ];
   const [skillState, setSkillState] = useState(skillContainers[0].value);
 
@@ -20,24 +20,25 @@ const Skills = (props: Props) => {
       <section className="w-[100vw]">
         <div className="hidden lg:flex w-3/5 h-[800px] mx-auto flex-col flex-wrap content-around gap-8">
           {skillContainers.map((box, index) => {
+            console.log('index=>', index, box.imgUrl);
             return (
               <div key={index} className="interactiveBox">
                 <div className="mx-auto font-bold text-yellow-600 text-3xl">{box.value}</div>
                 <div className="w-full mx-auto h-[1px] bg-black my-1"></div>
-                <img src={process.env.PUBLIC_URL + '/' + box.imgUrl} alt={box.value} />
+                <img src={box.imgUrl} alt={box.value} />
               </div>
             );
           })}
         </div>
 
-        <ul className="lg:hidden relative min-h-[200px] mx-auto w-[90vw] flex flex-col justify-center items-center">
+        <ul className="lg:hidden w-[360px] relative min-h-[500px] mx-auto flex flex-col justify-center items-center">
           {skillContainers.map((box, index) => {
             return (
               <li key={index} style={{ top: `${index * 50}px` }} className={`absolute left-0  w-[155px]`}>
                 <button
                   className={`w-full h-full ${
                     box.value === skillState && 'rounded-r-none bg-pink-200'
-                  } hover:rounded-r-none interactiveBox`}
+                  } hover:rounded-r-none interactiveBox hover:bg-pink-200`}
                   onClick={() => {
                     setSkillState(box.value);
                   }}
@@ -48,11 +49,11 @@ const Skills = (props: Props) => {
                 {box.value === skillState && (
                   <div
                     style={{ top: `${-index * 50}px` }}
-                    className="absolute w-[55vw] left-[143px] p-4 bg-white rounded-2xl shadow-lg hover:shadow-xl z-50"
+                    className="absolute w-[220px] left-[143px] p-4 bg-white rounded-2xl shadow-lg hover:shadow-xl z-50"
                   >
                     <div className="mx-auto font-bold text-yellow-600 text-2xl lg:text-3xl">{box.value}</div>
                     <div className="w-full mx-auto h-[1px] bg-black my-1"></div>
-                    <img src={process.env.PUBLIC_URL + '/' + box.imgUrl} alt={box.value} />
+                    <img src={box.imgUrl} alt={box.value} />
                   </div>
                 )}
               </li>
---
+++ src/static/images/skills/index.ts
@@ -0,0 +1,9 @@
+import communication from './communication.png';
+import deployment from './deployment.png';
+import frontend from './frontend.png';
+import backend from './backend.png';
+import versionControl from './versionControl.png';
+
+const SKILL_IMAGES = { frontend, backend, communication, deployment, versionControl };
+
+export default SKILL_IMAGES;
